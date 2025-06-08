9b14ac4caf09c6ccadd39902262ad8849ee0d492
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-08 09:58:07 +0700

    Feat(full_title): change syntax of full_title method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(title = "")
     base_title = "Ruby on Rails Tutorial Sample App"
-    if title.empty?
-      base_title
-    else
-      title + " | " + base_title
-    end
+    title.blank? ? base_title : "#{title} | #{base_title}"
   end
 end
