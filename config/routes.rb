3fc88dd86246386f93a1d06c5d0dfc40f91a1a2c
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-10 11:26:01 +0700

    Feat(routes): refactor get rotues to use resources & remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get "/help", to: "static_pages#help"
-  get "/about", to: "static_pages#about"
-  get "/contact", to: "static_pages#contact"
+  resources :static_pages, path: "", only: [] do
+    collection do
+      get :help
+      get :about
+      get :contact
+    end
+  end
   get "/signup", to: "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
