16b3a58baf0277c16d6fb067105c35f98db1f9cc
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-12 14:27:05 +0700

    Feat(chapter_8): finish learing chapter 8 basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
 class UserLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login fails when login with invalid email and password" do
     get login_path
     assert_template "sessions/new"
-    post login_path, params: { "session": {
+    post login_path, params: { session: {
       email: "",
       password: ""
     } }
@@ -12,5 +16,53 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login fails when login with invalid password" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: { session: {
+      email: @user.email,
+      password: ""
+    } }
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "valid when login with valid information" do
+    get login_path
+    post login_path, params: { session: {
+      email: @user.email,
+      password: "zzz111"
+    } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: {
+      email: @user.email,
+      password: "zzz111"
+    } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
