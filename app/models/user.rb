860b994af222ba6953cdd24d684cf02b7c562746
Nguyễn Hồ Bá Doãn <113108832+Naodab@users.noreply.github.com>
2025-06-13 17:08:40 +0700

    Finish chapter 6: modelling user. (#7)
    * Feat(chapter 6): finish chapter 6 modeling users
    
    * Feat(fixtures/users.yml): remove blank file
    
    * Feat(user): add validate password must be at least one letter and one digest
    
    * Chore(test command): change test command
    
    * Refactor(test): group and clarify User model validation tests
    
    * Feat(test): adjust number of test case
    
    * Fix(users): fix regex email address
    
    * Feat(test): update test case declarations

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d]+(-[a-z\d]+)*\.)+[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*[A-Za-z])(?=.*\d).+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
+  validates :password,
+            presence: true,
+            length: { minimum: 6 },
+            format: { with: VALID_PASSWORD_REGEX }
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,90 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                      password: "foobar2", password_confirmation: "foobar2")
+  end
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test "invalid name format edges case" do
+    invalid_names = [
+      "   ",
+      "a" * 51
+    ]
+    invalid_names.each do |name|
+      @user.name = name
+      assert_not @user.valid?
+    end
+  end
+
+  test "invalid email format edge cases" do
+    invalid_addresses = [
+      "",
+      "a" * 244 + "@example.com",
+      "user@.com",
+      "@example.com",
+      "user@example.",
+      "user@-example.com"
+    ]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "invalid with duplicate email" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "valid email addresses" do
+    valid_addresses = [
+      "user@example.com",
+      "USER@foo.COM",
+      "A_US-ER@foo.bar.org",
+      "first.last@foo.jp",
+      "alice+bob@baz.cn"
+    ]
+    valid_addresses.each do |valid_addresses|
+      @user.email = valid_addresses
+      assert @user.valid?, "#{valid_addresses.inspect} should be valid"
+    end
+  end
+
+  test "valid when email is saved as lower-case" do
+    mixed_case_email = "Foo@ExamPle.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "invalid without password" do
+    @user.password = @user.password_confirmation = "      "
+    assert_not @user.valid?
+  end
+
+  test "invalid when password is shorter than 6 characters" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "invalid when password contains no digits" do
+    @user.password = "a" * 6
+    assert_not @user.valid?
+  end
+
+  test "invalid when password contains no letters" do
+    @user.password = @user.password_confirmation = "1" * 6
+    assert_not @user.valid?
+  end
+
+  test "valid when password contains at least one letter and one digit" do
+    @user.password = @user.password_confirmation = "zzzz111"
+    assert @user.valid?
+  end
+end
