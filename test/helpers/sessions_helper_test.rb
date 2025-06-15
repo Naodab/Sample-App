9e79bce06c82441f9194eeffcde9cfc5eaf457de
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-15 14:51:02 +0700

    Feat(login): implement advanced login

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class SessionsHelperTest < ActionView::TestCase
+  def setup
+    @user = users(:michael)
+    remember_user @user
+  end
+
+  test "current returns right when sessions is nil" do
+    assert_equal @user, current_user
+    # assert is_logged_in?
+  end
+end
