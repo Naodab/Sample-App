699673538c13b8531ee0699dedc04284bd1278f8
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-16 17:02:43 +0700

    Fix: add rails-ujs for method: :delete

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
-//= link_directory ../../javascript .js
-//= link_directory ../../javascript/controllers .js
 //= link application.css
+//= link rails-ujs.js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
