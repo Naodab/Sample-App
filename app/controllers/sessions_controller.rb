60eeb2ef29f7e410d15b560ab38320d439a0185b
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-16 09:32:15 +0700

    Feat(chapter 10): finish chapter 10 update, index, delete users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       log_in user
       handle_remember_me user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
