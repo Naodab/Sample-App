ac51fd4ab955ac819ad86f03cd136e39d3f9363a
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-19 20:43:43 +0700

    Fix: refactor method's name

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -35,11 +35,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     user = assigns(:user)
     assert_not user.activated?
     log_in_as(user)
-    assert_not is_logged_in?
+    assert_not logged_in?
     get edit_account_activation_path('invalid token', email: user.email)
-    assert_not is_logged_in?
+    assert_not logged_in?
     get edit_account_activation_path(user.activation_token, email: 'wrong')
-    assert_not is_logged_in?
+    assert_not logged_in?
     get edit_account_activation_path(user.activation_token, email: user.email)
     assert user.reload.activated?
     follow_redirect!
