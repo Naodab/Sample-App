de5de1be41e81bc4ca8a71c9c2ce2805dd07a234
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-19 20:23:54 +0700

    Refactor: refactor code follow rubocop guide

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 class PasswordResetsController < ApplicationController
-  before_action :get_user, only: %i[edit update]
+  before_action :set_user, only: %i[edit update]
   before_action :valid_user, only: %i[edit update]
   before_action :check_expire, only: %i[edit update]
 
-  def new
-  end
+  def new; end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
-      render "new"
+      flash.now[:danger] = 'Email address not found'
+      render 'new'
     end
   end
 
@@ -26,33 +26,33 @@ class PasswordResetsController < ApplicationController
     if @user.update(user_params)
       log_in @user
       @user.forget :reset
-      flash[:success] = "Password has been reset."
+      flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
-      render "edit"
+      render 'edit'
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 
-    def get_user
-      @user = User.find_by(email: params[:email])
-    end
+  def set_user
+    @user = User.find_by(email: params[:email])
+  end
 
-    def valid_user
-      unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
-        redirect_to root_url
-      end
-    end
+  def valid_user
+    return if @user&.activated? && @user.authenticated?(:reset, params[:id])
 
-    def check_expire
-      if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired"
-        redirect_to new_password_reset_url
-      end
-    end
+    redirect_to root_url
+  end
+
+  def check_expire
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'Password reset has expired'
+    redirect_to new_password_reset_url
+  end
 end
--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
   def setup
@@ -6,35 +8,35 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "should not reset password when token has expired" do
+  test 'should not reset password when token has expired' do
     get new_password_reset_path
     post password_resets_path,
-    params: { password_reset: { email: @user.email } }
+         params: { password_reset: { email: @user.email } }
     @user = assigns(:user)
     @user.update_attribute(:reset_sent_at, 3.hours.ago)
     patch password_reset_path(@user.reset_token),
-    params: { email: @user.email,
-    user: { password: "foobar",
-    password_confirmation: "foobar" } }
+          params: { email: @user.email,
+                    user: { password: 'foobar',
+                            password_confirmation: 'foobar' } }
     assert_response :redirect
     follow_redirect!
-    assert_match "expired", response.body
+    assert_match 'expired', response.body
   end
 
-  test "password resets" do
+  test 'password resets' do
     get new_password_reset_url
-    assert_template "password_resets/new"
-    assert_select "input[name=?]", "password_reset[email]"
-    post password_resets_path, params: { password_reset: { email: "" } }
+    assert_template 'password_resets/new'
+    assert_select 'input[name=?]', 'password_reset[email]'
+    post password_resets_path, params: { password_reset: { email: '' } }
     assert_not flash.empty?
-    assert_template "password_resets/new"
+    assert_template 'password_resets/new'
     post password_resets_path, params: { password_reset: { email: @user.email } }
     assert_equal 1, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
     assert_redirected_to root_url
 
     user = assigns(:user)
-    get edit_password_reset_path(user.reset_token, email: "")
+    get edit_password_reset_path(user.reset_token, email: '')
     assert_redirected_to root_url
 
     user.toggle!(:activated)
@@ -42,22 +44,22 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     user.toggle!(:activated)
 
-    get edit_password_reset_path("wrong token", email: user.email)
+    get edit_password_reset_path('wrong token', email: user.email)
     assert_redirected_to root_url
 
     get edit_password_reset_path(user.reset_token, email: user.email)
-    assert_template "password_resets/edit"
-    assert_select "input[name=email][type=hidden][value=?]", user.email
+    assert_template 'password_resets/edit'
+    assert_select 'input[name=email][type=hidden][value=?]', user.email
 
     patch password_reset_path(user.reset_token),
-      params: { user: { password: "foo", password_confirmation: "bar" },
-                email: user.email }
-    assert_select "div#error_explanation"
+          params: { user: { password: 'foo', password_confirmation: 'bar' },
+                    email: user.email }
+    assert_select 'div#error_explanation'
 
     patch password_reset_path(user.reset_token),
-      params: { user: { password: "zzz111", password_confirmation: "zzz111" },
-                email: user.email }
-    assert is_logged_in?
+          params: { user: { password: 'zzz111', password_confirmation: 'zzz111' },
+                    email: user.email }
+    assert logged_in?
     assert_not flash.empty?
     assert_redirected_to user
   end
