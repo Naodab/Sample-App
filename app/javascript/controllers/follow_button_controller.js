a794bf38c12ebd72b1be752e555affaf896e5ed0
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-24 11:34:47 +0700

    Fix: fix disable follow/unfollow button

--- app/javascript/controllers/follow_button_controller.js
+++ app/javascript/controllers/follow_button_controller.js
@@ -1,8 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    disable(event) {
-        const button = event.target;
-        button.classList.add("disable");
+    disableOnSubmit() {
+        const button = this.element.querySelector("input[type=submit], button[type=submit]");
+        if (button) {
+            button.classList.add("disable");
+            button.value = "Processing...";
+            button.disabled = true;
+        }
     }
 }
